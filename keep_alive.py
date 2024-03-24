fb30af057f6fca421bfc4d1e92bed6a20c9e64a7
asiedisaskeriz <68537410+asiedisaskeriz@users.noreply.github.com>
2024-03-24 19:16:47 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask
 from threading import Thread
-app = Flask(__name__)
+
+app = Flask('')
+
 @app.route('/')
-def index():
-    return "Alive"
+def main():
+    return '<meta http-equiv="refresh" content="0; URL=https://phantom.fr.to/support"/>'
+
 def run():
-  app.run(host='0.0.0.0',port=8080)
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
