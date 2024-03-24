b5b6d0813e0271b2eecb931bea448b2777a0c7bb
asiedisaskeriz <68537410+asiedisaskeriz@users.noreply.github.com>
2024-03-24 21:45:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,20 +29,6 @@ username = userinfo["username"]
 discriminator = userinfo["discriminator"]
 userid = userinfo["id"]
 
-def on_open(ws):
-    auth = {"op": 2,"d": {"token": usertoken,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
-    vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
-    ws.send(json.dumps(auth))
-    ws.send(json.dumps(vc))
-    time.sleep(30)
-    ws.close()
-
-def on_error(ws, error):
-    print(f"[ERROR] {error}")
-
-def on_close(ws):
-    print("[INFO] Connection closed.")
-
 def on_message(ws, message):
     print(f"[INFO] Received message: {message}")
 
@@ -50,11 +36,8 @@ def run_joiner():
     os.system("clear")
     print(f"Logged in as {username}#{discriminator} ({userid}).")
     while True:
-        ws = websocket.WebSocket()
-        ws.on_message = on_message
-        ws.on_error = on_error
-        ws.on_close = on_close
-        ws.connect('wss://gateway.discordapp.com/?v=9&encoding=json')
+        ws = websocket.WebSocketApp('wss://gateway.discordapp.com/?v=9&encoding=json',
+                                    on_message=on_message)
         ws.run_forever()
 
 run_joiner()
